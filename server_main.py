b2bc4242f777880b3fa188f5d1a8b6d898e18f22
LuisJCruz <luis.cruz103@upr.edu>
2026-05-23 18:14:41 -0400

    Refactor environment variable handling for auction duration and expected clients

--- server_main.py
+++ server_main.py
@@ -1,7 +1,12 @@
+import os
+import server_functions
 from server_functions import start_server, open_log_file, close_log_file
 
 
 def main():
+    server_functions.AUCTION_DURATION = int(os.environ.get("AUCTION_DURATION", 20))
+    server_functions.EXPECTED_CLIENTS = int(os.environ.get("EXPECTED_CLIENTS", 3))
+
     open_log_file("server.log")
     try:
         start_server()
