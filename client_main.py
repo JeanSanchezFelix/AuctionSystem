c68cc8a5417fce6c9807133c5a9a27de6122ff52
LuisJCruz <luis.cruz103@upr.edu>
2026-05-23 14:06:50 -0400

    Enhance message handling in client functions and improve log file naming

--- client_main.py
+++ client_main.py
@@ -1,9 +1,10 @@
 import sys
+import time
 from client_functions import start_client, open_log_file, close_log_file
 
 
 def main():
-    log_name = "client.log"
+    log_name = f"client_{int(time.time() * 1000)}.log"
 
     if len(sys.argv) > 1:
         log_name = sys.argv[1]
