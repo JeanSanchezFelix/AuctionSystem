f921f8b92d614b30ef0e4003c878a58a3e177a0a
JeanSanchezFelix <jeanpiere.sanchez@upr.edu>
2026-05-24 02:55:57 +0000

    100%

--- server_main.py
+++ server_main.py
@@ -1,8 +1,13 @@
-
+import os
+import server_functions
 from server_functions import start_server, open_log_file, close_log_file
 
 
+
+
 def main():
+    server_functions.AUCTION_DURATION = int(os.getenv("AUCTION_DURATION", 20))
+    server_functions.EXPECTED_CLIENTS = int(os.getenv("EXPECTED_CLIENTS", 3))
     open_log_file("server.log")
     try:
         start_server()
